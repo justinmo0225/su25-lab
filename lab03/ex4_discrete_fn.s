d8855801217e64f80e6e68f2cffd6d3472c190a5
DM <DM>
2025-07-10 22:25:45 -0700

    ex4

--- lab03/ex4_discrete_fn.s
+++ lab03/ex4_discrete_fn.s
@@ -5,20 +5,17 @@
 # a1 is the address of the "output" array (read the lab spec for more information).
 # The return value should be stored in a0
 .data
-values: .word 6, 61, 17, -38, 19, 42, 5     # array
+targets: .word 6, 61, 17, -38, 19, 42, 5
 
 .text
 f:
     # Your code here
-
-    addi t0, a0, 3      # -3 to 3 becomes 0 to 6
-    slli t1, t0, 2      # makes each value 4 bytes
-    la t2, values
-    add t3, t1, t2      # temporary to hold t1 + t2
-    lw t4, 0(t3)        # t4 = output
-    add t5, a1, t1      # placeholder for a1 + t1
-    sw t6, 0(t5)
+    la t1 targets
+    addi t0 a0 3
+    slli t0 t0 2
+    add t0 t0 t1
+    lw a0 0(t0)
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
-    jr ra
+    ret
