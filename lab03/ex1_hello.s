f45fc08d84eee0f5b9fb1327bf0e3580f3c5f079
DM <DM>
2025-07-10 17:38:53 -0700

    ex3 test 3

--- lab03/ex1_hello.s
+++ lab03/ex1_hello.s
@@ -1,5 +1,6 @@
 .text
 
+addi a0, x0, 1
 addi a1 x0 2025
 
 # This prints out the integer stored in a1
--- lab03/ex3_memcheck.s
+++ lab03/ex3_memcheck.s
@@ -22,7 +22,7 @@ loop:
     addi t0 t0 4
     # Check if we are done
     # If not, loop
-    blt t1 t2 loop
+    bge t2 t1 loop
 
     # Exit the program
     li a0 0
