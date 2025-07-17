4fc1c84595f102a4df438cf3909ae4365b652385
DM <DM>
2025-07-16 21:11:29 -0700

    hopefully last test

--- lab04/ex3.s
+++ lab04/ex3.s
@@ -32,6 +32,8 @@ main:
 #     where ^ is the exponent operator, not XOR
 ex3:
     # Note: Add code BELOW without altering existing lines.
+    addi sp, sp, -4
+    sw ra 0(sp)         # saves return arg
 
     # return 1 if a1 == 0
     beq a1 x0 ex3_zero_case
@@ -53,4 +55,9 @@ ex3_zero_case:
     li a0 1
 
 ex3_end:
+
+    lw ra 0(sp)
+    addi sp, sp, 4
+
+
     jr ra
